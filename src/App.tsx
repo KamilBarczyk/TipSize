c262bb6b350faab2ca9d92d045056f09df294d9c
KamilBarczyk <62600472+KamilBarczyk@users.noreply.github.com>
2024-11-09 19:48:19 +0100

    Applied basic styling to foundational page structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import './App.css'
 import Display from './components/Display'
 import Form from './components/Form'
 
+/* TODO: Add logo here to the photo displayed at the top */
+// import TipSize from "./assets/images/TipSize.png"
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="wrapper">
+      {/* <img src={TipSize} alt="Splitter Logo"/> */}
       <div className="container">
         <Form />
         <Display />
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,25 +1,23 @@
 const Display = () => {
     return (
-        <div>
-            <div className="display-row">
-                <div>
+        <div className="display">
+            <div className="display-group">
+                <div className="display-row">
                     <div className="display-label">
                         <p className="header">Tip Amount</p>
                         <p className="unit">/ person</p>
                     </div>
-                    <div className="display-amt">
-                        <p className="value">$0.00</p>
-                    </div>
-                    <div className="display-label">
-                        <p className="header">Tip Amount</p>
-                        <p className="unit">/ person</p>
-                    </div>
-                    <div className="display-amt">
-                        <p className="value">$0.00</p>
-                    </div>
+                    <p className="display-amt">$0.00</p>
                 </div>
-                <button className="btn">Reset</button>
+                <div className="display-row">
+                    <div className="display-label">
+                        <p className="header">Tip Amount</p>
+                        <p className="unit">/ person</p>
+                    </div>
+                    <p className="display-amt">$0.00</p>
+                </div>
             </div>
+            <button className="btn">Reset</button>
         </div>
     )
 }
